a91291deb605e02fca7016bd405c85655995560a
clodeg <clodeg@stu.otc.school.nz>
2020-09-02 11:00:51 +1200

    Added lots of dialouge, as well as a few functions, etc.

--- plane_ver1.py
+++ plane_ver1.py
@@ -1,13 +1,19 @@
 # THIS IS A TEST
 import time
 import random
+time.sleep(3)
+time.sleep(2)
+time.sleep(1)
 
 
 def playtutorial():
     print("This game will inform you on how to better your chances of surviving a plane crash.")
+    time.sleep(2)
     print("For more information, visit https://www.cbsnews.com/news/how-to-survive-a-plane-crash/")
+    time.sleep(2)
     print("\n")
     print("How to survive a plane crash! (Created by Griffin Chode)")
+    time.sleep(2)
     playtutorial1()
 
 def playtutorial1():
@@ -15,35 +21,42 @@ def playtutorial1():
         playtutorialchoice = int(input("Would you like to go play through the tutorial? yes(1) no(2) :"))
         if playtutorialchoice == 1:
             print("Ok.")
+            time.sleep(2)
             tutorial1()
         elif playtutorialchoice == 2:
             print("Ok.")
+            time.sleep(2)
             tutorial2()
         else:
-            print("Invalid answer, try again.")
+            print("1Invalid answer, try again.")
             playtutorial1()
     except:
-        print("Invalid answer, try again.")
+        print("2Invalid answer, try again.")
         playtutorial1()
 
 
 def tutorial1():
     print("This game involves multiple choice questions.")
+    time.sleep(2)
     print("To select one of the choices, you must choose the number that is assigned to that choice.")
+    time.sleep(3)
     print("A test question will now be asked.")
+    time.sleep(2)
     try:
         test1 = int(input("Would you like to play the game? yes(1) no(2) :"))
         if test1 == 1:
             print("Great.")
+            time.sleep(2)
             print("Next, your name will be asked.")
+            time.sleep(2)
             tutorial2()
         elif test1 == 2:
             tutorial3()
         else:
-            print("Invalid answer, try again.")
+            print("3Invalid answer, try again.")
             tutorial1()
     except:
-        print("Invalid answer, try again.")
+        print("4Invalid answer, try again.")
         tutorial1()
 
 
@@ -64,10 +77,10 @@ def tutorial3():
             print("Ok, you will now be redirected to last question.")
             tutorial1()
         else:
-            print("Invalid answer, try again.")
+            print("5Invalid answer, try again.")
             tutorial3()
     except:
-        print("Invalid answer, try again.")
+        print("6Invalid answer, try again.")
         tutorial3()
 
 
@@ -87,7 +100,7 @@ def part1sub1(name):
     print("'Please make your way onto the plane.'")
     print("You go through the metal detector, and come out without any trouble.")
     print("Next, you make your way to the ticket issuer.")
-    print("With an authoratative tone of voice, he asks;")
+    print("With an authoritative tone of voice, he asks;")
     print("'What part of the plane would you like to be seated in, {}?'".format(name))
     part1sub2(name)
 
@@ -113,10 +126,10 @@ def part1sub2(name):
             print("far higher.")
             part1sub3back(name)
         else:
-            print("Invalid answer, try again.")
+            print("7Invalid answer, try again.")
             part1sub2(name)
     except:
-        print("Invalid answer, try again.")
+        print("8Invalid answer, try again.")
         part1sub2(name)
 
 
@@ -130,10 +143,10 @@ def part1sub3front(name):
         elif waitfornextflight == 2:
             part1sub5_title_notwaitingfornextflightfront(name)
         else:
-            print("Invalid answer, try again.")
+            print("9Invalid answer, try again.")
             part1sub3front(name)
     except:
-        print("Invalid answer, try again.")
+        print("10Invalid answer, try again.")
         part1sub3front(name)
 
 
@@ -147,10 +160,10 @@ def part1sub3middle(name):
         elif waitfornextflight == 2:
             part1sub5_title_notwaitingfornextflightmiddle(name)
         else:
-            print("Invalid answer, try again.")
+            print("11Invalid answer, try again.")
             part1sub3middle(name)
     except:
-        print("Invalid answer, try again.")
+        print("12Invalid answer, try again.")
         part1sub3middle(name)
 
 
@@ -164,10 +177,10 @@ def part1sub3back(name):
         elif waitfornextflight == 2:
             part1sub5_title_notwaitingfornextflightback(name)
         else:
-            print("Invalid answer, try again.")
+            print("13Invalid answer, try again.")
             part1sub3back(name)
-    except :
-        print("Invalid answer, try again.")
+    except:
+        print("14Invalid answer, try again.")
         part1sub3back(name)
 
 
@@ -187,8 +200,8 @@ def part1sub5_title_notwaitingfornextflightfront(name):
     print("You decide not to wait.")
     print("A flight attendant greets you as you board the plane.")
     print("Good morning {}, please make your way to the front of the plane.".format(name))
-    print("Your seat number is {}.".format(seat_numberfront))
-    playagain()
+    print("Your seat number is section A, {}.".format(seat_numberfront))
+    part2sub1(name)
 
 
 def part1sub5_title_notwaitingfornextflightmiddle(name):
@@ -196,8 +209,8 @@ def part1sub5_title_notwaitingfornextflightmiddle(name):
     print("You decide not to wait.")
     print("A flight attendant greets you as you board the plane.")
     print("Good morning {}, please make your way to the middle of the plane.".format(name))
-    print("Your seat number is {}.".format(seat_numbermiddle))
-    playagain()
+    print("Your seat number is section B, {}.".format(seat_numbermiddle))
+    part2sub1(name)
 
 
 def part1sub5_title_notwaitingfornextflightback(name):
@@ -205,9 +218,69 @@ def part1sub5_title_notwaitingfornextflightback(name):
     print("You decide not to wait.")
     print("A flight attendant greets you as you board the plane.")
     print("Good morning {}, please make your way to the back of the plane.".format(name))
-    print("Your seat number is {}.".format(seat_numberback))
-    playagain()
+    print("Your seat number is section C, {}.".format(seat_numberback))
+    part2sub1(name)
 
+def part2sub1(name):
+    print("You make yourself comfortable in your seat.")
+    print("The plane begins to take off with a large rumbling sound.")
+    print("Strange. You didn't expect it to sound like that.")
+    print("You ignore it, and lie back in the chair...")
+    print("The sudden sound of boisterous children awake you.")
+    print("You had fallen asleep.")
+    print("'Hello {}, would you like a glass of water?', the flight attendant asks.".format(name))
+    print("You try to reply, but your throat is raspy and sore.")
+    print("'Ok, you obviously need water. Here you go.'")
+    print("You take the glass of water, and have a phat chug of it.")
+    print("The urge to relieve yourself in the restroom is intense.")
+    print("RUN to the bathroom!")
+    print("children are in your way!")
+    print("go left to get around them, or right?")
+    part2sub2()
+
+def part2sub2():
+    part2sub2choice1 = int(input("left(1) right(2) :"))
+    try:
+        if part2sub2choice1 == 1:
+            print("You turn left and keep running...")
+            print("a SUITCASE falls on you!")
+            print("You begin running again, obviously at a slower rate.")
+            part2sub3()
+        elif part2sub2choice1 == 2:
+            print("You dodge to the right...")
+            print("Hitting an old lady on her way to ask the flight attendant")
+            print("if she could have a choco bikkie.")
+            print("You trample over her.")
+            part2sub3()
+        else:
+            print("15Invalid answer, try again.")
+            part2sub2()
+    except:
+        print("16Invalid answer, try again.")
+        part2sub2()
+
+def part2sub3():
+    print("The flight attendant is in your way!")
+    print("Run around her, or ask her to move out of the way?")
+    part2sub3choice1 = int(input("Run around(1) Ask to move(2) :"))
+    try:
+        if part2sub3choice1 == 1:
+            print("You try to run around the flight attendant,")
+            print("but fail miserably. You fall, hitting the side of")
+            print("a chair, cracking your skull.")
+            print("You die. Next time, try to be more polite.")
+            playagain()
+        elif part2sub3choice1 == 2:
+            print("You politely ask the flight attendant to move out of the way.")
+            print("She does, and passes you a choco bikkie on your way.")
+            print("You manage to get to the toilet and relieve yourself.")
+            playagain()
+        else:
+            print("17Invalid answer, try again.")
+            part2sub3()
+    except:
+        print("18Invalid answer, try again.")
+        part2sub3()
 
 # PlayAgain
 def playagain():
@@ -218,13 +291,12 @@ def playagain():
             playtutorial()
         elif restart == 2:
             print("Quitting...")
-            exit()
+            quit()
         else:
-            print("1Invalid answer, try again.")
+            print("15Invalid answer, try again.")
             playagain()
     except TypeError:
-        print("2Invalid answer, try again.")
+        print("16Invalid answer, try again.")
         playagain()
-
 
 playtutorial()
